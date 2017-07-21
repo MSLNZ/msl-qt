551e99218197bc479efb843c55b704408813a55e
jborbely <joe.borbely@gmail.com>
2017-07-22 08:58:06 +1200

    create application function

--- msl/qt/__init__.py
+++ msl/qt/__init__.py
@@ -1,4 +1,7 @@
+import sys
 from collections import namedtuple
+
+from PyQt5 import QtWidgets
 
 __author__ = 'Joseph Borbely'
 __copyright__ = '\xa9 2017, ' + __author__
@@ -6,3 +9,21 @@ __version__ = '0.1.0'
 
 version_info = namedtuple('version_info', 'major minor micro')(*map(int, __version__.split('.')[:3]))
 """:obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro) tuple."""
+
+
+def application(args=sys.argv):
+    """Returns the QApplication instance (creating one if necessary).
+
+    Parameters
+    ----------
+    args : :obj:`list` of :obj:`str`
+        A list of arguments to initialize the QApplication.
+
+    Returns
+    -------
+    The QApplication instance.
+    """
+    app = QtWidgets.QApplication.instance()
+    if app is None:
+        app = QtWidgets.QApplication(args)
+    return app
