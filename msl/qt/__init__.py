b5f0904fc672ebd6c0a5dfe24d297fd7106513ff
jborbely <joe.borbely@gmail.com>
2017-07-22 12:17:49 +1200

    change signature -- application(args=None)

--- msl/qt/__init__.py
+++ msl/qt/__init__.py
@@ -1,3 +1,6 @@
+"""
+Custom `Qt <https://wiki.python.org/moin/PyQt>`_ components for the UI.
+"""
 import sys
 from collections import namedtuple
 
@@ -11,19 +14,21 @@ version_info = namedtuple('version_info', 'major minor micro')(*map(int, __versi
 """:obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro) tuple."""
 
 
-def application(args=sys.argv):
+def application(args=None):
     """Returns the QApplication instance (creating one if necessary).
 
     Parameters
     ----------
     args : :obj:`list` of :obj:`str`
         A list of arguments to initialize the QApplication.
+        If :obj:`None` then uses :obj:`sys.argv`.
 
     Returns
     -------
-    The QApplication instance.
+    :obj:`QtWidgets.QApplication`
+        The QApplication instance.
     """
     app = QtWidgets.QApplication.instance()
     if app is None:
-        app = QtWidgets.QApplication(args)
+        app = QtWidgets.QApplication(sys.argv if args is None else args)
     return app
