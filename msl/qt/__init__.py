ebb605c2a7565029764a429a6246bf3bf8ba59cc
j.borbely <joe.borbely@gmail.com>
2017-08-07 14:09:27 +1200

    rename function to image_to_base64; can now load icon from DLL/EXE

--- msl/qt/__init__.py
+++ msl/qt/__init__.py
@@ -3,6 +3,7 @@ General convenience functions and package constants.
 """
 import os
 import sys
+import base64
 from collections import namedtuple
 
 from PyQt5 import QtWidgets, QtGui, QtCore
@@ -53,15 +54,53 @@ def icon(obj):
     Parameters
     ----------
     obj : :obj:`object`
-        The object to be converted. The data type of `obj` can be one of:
+        The object to be converted to a :obj:`QIcon`. The data type of `obj` can be one of:
 
         * :obj:`QIcon`
         * :obj:`QPixmap`
-        * :obj:`QStyle.StandardPixmap`
-        * :obj:`bytes`, :obj:`bytearray` or :obj:`QtCore.QByteArray`: See also :func:`image_to_bytes`
-        * :obj:`str`: The path to an image file. If only the filename is specified
-          then searches :obj:`sys.path` and :obj:`os.environ['PATH'] <os.environ>`
-          for the image file.
+        * :obj:`QStyle.StandardPixmap`:
+          See `StandardPixmap <http://doc.qt.io/qt-5/qstyle.html#StandardPixmap-enum>`_
+          for the possible enum values. Example::
+
+              msl.qt.icon(PyQt5.QtWidgets.QStyle.SP_TitleBarMenuButton)
+
+        * :obj:`bytes`: A `Base64 <https://en.wikipedia.org/wiki/Base64>`_ representation
+          of an encoded image. See also :func:`image_to_base64`.
+        * :obj:`str`: The path to an image file or an icon embedded in a Windows DLL/EXE file.
+          If `obj` is a path to an image file and if only the filename is specified then the
+          directories in :obj:`sys.path` and :obj:`os.environ['PATH'] <os.environ>` are used
+          to search for the image file. If `obj` refers to an icon in a Windows DLL/EXE file
+          then `obj` is the path to the DLL/EXE file and the icon index separated by the ``|``
+          character. An overview of some Windows DLL/EXE files which contain icons can be found
+          `here <http://www.digitalcitizen.life/where-find-most-windows-10s-native-icons>`_.
+
+          The following examples illustrate the various ways to request an icon by passing
+          in a :obj:`str`-type argument::
+
+              # provide the full path to image files
+              msl.qt.icon('D:/code/resources/icons/msl.png')
+              msl.qt.icon('D:/code/resources/icons/photon.png')
+
+              # insert the folder where the icons are located into sys.path
+              sys.path.insert(0, 'D:/code/resources/icons/')
+              # so now only the filename needs to be specified to load the icon
+              msl.qt.icon('msl.png')
+              msl.qt.icon('photon.png')
+
+              # load icon 23 from the Windows shell32.dll file
+              msl.qt.icon('C:/Windows/System32/shell32.dll|23')
+
+              # load icon 0 from the Windows explorer.exe file
+              msl.qt.icon('C:/Windows/explorer.exe|0')
+
+              # by default it is assumed that the icon is located in a file in one of two directories:
+              # a DLL file in C:/Windows/System32/ or an EXE file in C:/Windows/
+              # so the following is a simplified way to load an icon in a Windows DLL file
+              msl.qt.icon('shell32|23')
+              msl.qt.icon('imageres|1')
+              msl.qt.icon('compstui|51')
+              # and the following is a simplified way to load an icon in a Windows EXE file
+              msl.qt.icon('explorer|0')
 
     Returns
     -------
@@ -70,7 +109,7 @@ def icon(obj):
 
     Raises
     ------
-    :obj:`FileNotFoundError`
+    :obj:`IOError`
         If `obj` is of type :obj:`str` and the file cannot be found.
     :obj:`TypeError`
         If the data type of `obj` is not supported.
@@ -78,6 +117,9 @@ def icon(obj):
     if isinstance(obj, QtGui.QIcon):
         return obj
     elif isinstance(obj, str):
+        if '|' in obj:  # then loading an icon from a Windows DLL/EXE file
+            return icon(image_to_base64(obj))
+        # otherwise loading an icon from an image file
         if os.path.isfile(obj):
             return QtGui.QIcon(obj)
         search_paths = sys.path + os.environ['PATH'].split(os.pathsep)
@@ -85,57 +127,133 @@ def icon(obj):
             full_path = os.path.join(path, obj)
             if os.path.isfile(full_path):
                 return QtGui.QIcon(full_path)
-        raise FileNotFoundError("icon(obj): Cannot find '{}'".format(obj))
+        raise FileNotFoundError("Cannot find image file '{}'".format(obj))
     elif isinstance(obj, QtWidgets.QStyle.StandardPixmap):
         return QtGui.QIcon(application().style().standardIcon(obj))
     elif isinstance(obj, QtGui.QPixmap):
         return QtGui.QIcon(obj)
-    elif isinstance(obj, (bytes, bytearray, QtCore.QByteArray)):
+    elif isinstance(obj, bytes):
         img = QtGui.QImage()
-        if not isinstance(obj, QtCore.QByteArray):
-            img.loadFromData(QtCore.QByteArray.fromBase64(obj))
-        else:
-            img.loadFromData(obj)
+        img.loadFromData(QtCore.QByteArray.fromBase64(obj))
         return QtGui.QIcon(QtGui.QPixmap.fromImage(img))
     else:
-        raise TypeError("icon(obj): argument has unexpected type '{}'".format(type(obj).__name__))
+        raise TypeError("Argument has unexpected type '{}'".format(type(obj).__name__))
 
 
-def image_to_bytes(image, size=None, mode=QtCore.Qt.KeepAspectRatio, fmt='PNG'):
-    """Return the byte representation of an image.
+def image_to_base64(image, size=None, mode=QtCore.Qt.KeepAspectRatio, fmt='PNG'):
+    """Encode the image using Base64_ and return the encoded :obj:`bytes`.
 
-    This function is useful if you wanted to save images in a database or if you
-    wanted to use images in your GUI and rather than loading the images from a
-    file on the hard disk you define your images in a Python module as :obj:`bytes`
-    objects. Loading the images from the hard disk means that you must also distribute
-    the images with your Python code if you share your code.
+    This function is useful if you want to save images in a database or if you
+    want to use images in your GUI and rather than loading images from a
+    file on the hard disk you define your images in a Python module as a Base64_
+    :obj:`bytes` variable. Loading the images from the hard disk means that you
+    must also distribute the images with your Python code if you share your code.
+
+    .. _Base64: https://en.wikipedia.org/wiki/Base64
 
     Parameters
     ----------
     image : :obj:`object`
         An image with a data type that is handled by :func:`icon`.
-    size : :obj:`int`, :obj:`tuple` of :obj:`int` or :obj:`QSize`
-        Rescale the image to the specified `size` before converting it to :obj:`bytes`.
-        If :obj:`None` then do not rescale the image. If a :obj:`tuple` then the
-        (width, height) values.
+    size : :obj:`float`, :obj:`tuple` of :obj:`int` or :obj:`QSize`
+        Rescale the image to the specified `size` before converting it to Base64_.
+        If :obj:`None` then do not rescale the image. If a :obj:`float` then a scaling
+        factor, if a :obj:`tuple` then the (width, height) values.
     mode : :obj:`Qt.AspectRatioMode`
         How to maintain the aspect ratio if rescaling. Allowed values are
         :obj:`Qt.IgnoreAspectRatio`, :obj:`Qt.KeepAspectRatio` or
         :obj:`Qt.KeepAspectRatioByExpanding`. The default mode is :obj:`Qt.KeepAspectRatio`.
     fmt : :obj:`str`
-        The image format to use when converting.
+        The image format to use when converting. The supported values are: ``BMP``,
+        ``JPG``, ``JPEG`` and ``PNG``.
 
     Returns
     -------
     :obj:`bytes`
-        The byte representation of the image.
+        The Base64_ representation of the image encoded as :obj:`bytes`.
+
+    Raises
+    ------
+    :obj:`IOError`
+        If the image file cannot be found.
+    :obj:`ValueError`
+        If the image format, `fmt`, to use for converting is not supported.
     """
+    fmt = fmt.upper()
+    ALLOWED_FORMATS = ['BMP', 'JPG', 'JPEG', 'PNG']
+    if fmt not in ALLOWED_FORMATS:
+        raise ValueError('Invalid format {}. Must be one of: {}'.format(fmt, ', '.join(ALLOWED_FORMATS)))
+
+    if isinstance(image, str) and '|' in image:
+        # extract an icon from a Windows DLL/EXE file
+        # uses ctypes and the .NET Framework to convert the icon to base64
+        # import here in case pythonnet is not installed
+        import clr
+        import ctypes
+
+        shell32 = ctypes.windll.shell32
+
+        img_fmts = {
+            'BMP':  clr.System.Drawing.Imaging.ImageFormat.Bmp,
+            'JPG':  clr.System.Drawing.Imaging.ImageFormat.Jpeg,
+            'JPEG': clr.System.Drawing.Imaging.ImageFormat.Jpeg,
+            'PNG':  clr.System.Drawing.Imaging.ImageFormat.Png,
+        }
+
+        s = image.split('|')
+        path = s[0]
+        icon_index = int(s[1])
+        if icon_index < 0:
+            raise ValueError('The icon index must be >= 0')
+
+        if not os.path.isfile(path):
+            err_msg = "Cannot find DLL/EXE file '{}'".format(s[0])
+            if os.path.split(path)[0]:  # then it wasn't just the filename that was specified
+                raise FileNotFoundError(err_msg)
+
+            filename = os.path.splitext(os.path.basename(path))[0]
+            path = 'C:/Windows/System32/{}.dll'.format(filename)
+            if not os.path.isfile(path):
+                path = 'C:/Windows/{}.exe'.format(filename)
+                if not os.path.isfile(path):
+                    raise FileNotFoundError(err_msg)
+
+        # extract the handle to the "large" icon
+        path_ptr = ctypes.c_char_p(path.encode())
+        handle_large = ctypes.c_int()
+        res = shell32.ExtractIconExA(path_ptr, icon_index, ctypes.byref(handle_large), ctypes.c_void_p(), 1)
+        if res != 1:
+            # Check if the icon index is valid
+            max_index = shell32.ExtractIconExA(path_ptr, -1, ctypes.c_void_p(), ctypes.c_void_p(), 0) - 1
+            if icon_index > max_index:
+                msg = 'Requested icon {}, the maximum icon index allowed is {}'.format(icon_index, max_index)
+            else:
+                msg = "ExtractIconExA: Cannot extract icon {} from '{}'".format(icon_index, path)
+            raise IOError(msg)
+
+        # get the icon bitmap and convert it to base64
+        handle = clr.System.Int32(handle_large.value)
+        handle_ptr = clr.System.IntPtr.op_Explicit(handle)
+        stream = clr.System.IO.MemoryStream()
+        bmp = clr.System.Drawing.Icon.FromHandle(handle_ptr).ToBitmap()
+        bmp.Save(stream, img_fmts[fmt])
+        base = base64.b64encode(bytes(stream.GetBuffer()))
+
+        # clean up
+        ctypes.windll.user32.DestroyIcon(handle_large)
+        stream.Dispose()
+        return base
+
     icon_ = icon(image)
-    default_size = icon_.availableSizes()[0]
+    default_size = icon_.availableSizes()[-1]  # use the largest size as the default size
+    pixmap = icon_.pixmap(default_size)
+
     if size is None:
         size = default_size
-    elif isinstance(size, (int, float)):
+    elif isinstance(size, int):
         size = QtCore.QSize(size, size)
+    elif isinstance(size, float):
+        size = QtCore.QSize(int(default_size.width()*size), int(default_size.height()*size))
     elif isinstance(size, (list, tuple)):
         if len(size) == 0:
             size = default_size
@@ -143,7 +261,10 @@ def image_to_bytes(image, size=None, mode=QtCore.Qt.KeepAspectRatio, fmt='PNG'):
             size = QtCore.QSize(size[0], size[0])
         else:
             size = QtCore.QSize(size[0], size[1])
-    pixmap = icon_.pixmap(default_size).scaled(size, aspectRatioMode=mode)
+
+    if (size.width() != default_size.width()) or (size.height() != default_size.height()):
+        pixmap = pixmap.scaled(size, aspectRatioMode=mode)
+
     array = QtCore.QByteArray()
     buffer = QtCore.QBuffer(array)
     buffer.open(QtCore.QIODevice.WriteOnly)
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import pytest
 from PyQt5 import QtWidgets, QtCore, QtGui
 
@@ -11,17 +14,19 @@ def test_icon():
     assert isinstance(qt.icon(QtGui.QPixmap()), QtGui.QIcon)
     assert isinstance(qt.icon(QtWidgets.QStyle.SP_TitleBarMenuButton), QtGui.QIcon)
 
-    logo = b'iVBORw0KGgoAAAANSUhEUgAAACgAAABCCAYAAAAlkZRRAAAACXBIWXMAAAsTAAALEwEAmpwYAAABpElEQVR' \
-           b'oge2ZT07CQBSHf29kLXKOsnApBEx7DAl3sT2BZzAx6C1ahYBLFzRxyQ1Q12SeiwoSQonjzMiLmW9Fh+nMlz' \
-           b'ft/HkFhEOuGnofRLz+3RyVztpVrhryhXjBhu8OlsN2DADQ/NYalS+m93sXVJpzACBGASAxvt+1kGuCoC3On' \
-           b'kGXc9824iMYBG0JgrZ4X0lMWe+KtKKkdTcvQgT3sRy2Y6URr6+bo3laV/cogpUcX28VpbV1vdtY4iyCYcsv' \
-           b'lSBoi7j94G474iMoXjDMg7YEQVvEC4rPDxq/xctBdH7CuAGA0/vSOBlkivk0o+iMNcfuVWq6+6uOfot4wdo' \
-           b'h/riKcqbqYOMrMfQTxEdQvKC4/eAu4iMYBG0RL9gAthd6yg4lco6B+AiKFzSfB1erBVQj8+CyF2PB1sPrAg' \
-           b'fyea4RP8TiBb+GmDIA0ArF5h/CLUCPx5AKuISAsJJYIV4w8O8hAOj2O9VbTJRNn6YpAHT6nZxQnYun49nmQ' \
-           b'NTrXcSaKN8t37RRU85AMRvPEgDoXnZT8Pe3un31FXMymTzL/xwrXlA8n2MHdwPYAbB5AAAAAElFTkSuQmCC'
-    assert isinstance(qt.icon(logo), QtGui.QIcon)
-    assert isinstance(qt.icon(bytearray(logo)), QtGui.QIcon)
-    assert isinstance(qt.icon(QtCore.QByteArray(logo)), QtGui.QIcon)
+    base64 = b'iVBORw0KGgoAAAANSUhEUgAAACgAAABCCAYAAAAlkZRRAAAACXBIWXMAAAsTAAALEwEAmpwYAAABpElEQVR' \
+             b'oge2ZT07CQBSHf29kLXKOsnApBEx7DAl3sT2BZzAx6C1ahYBLFzRxyQ1Q12SeiwoSQonjzMiLmW9Fh+nMlz' \
+             b'ft/HkFhEOuGnofRLz+3RyVztpVrhryhXjBhu8OlsN2DADQ/NYalS+m93sXVJpzACBGASAxvt+1kGuCoC3On' \
+             b'kGXc9824iMYBG0JgrZ4X0lMWe+KtKKkdTcvQgT3sRy2Y6URr6+bo3laV/cogpUcX28VpbV1vdtY4iyCYcsv' \
+             b'lSBoi7j94G474iMoXjDMg7YEQVvEC4rPDxq/xctBdH7CuAGA0/vSOBlkivk0o+iMNcfuVWq6+6uOfot4wdo' \
+             b'h/riKcqbqYOMrMfQTxEdQvKC4/eAu4iMYBG0RL9gAthd6yg4lco6B+AiKFzSfB1erBVQj8+CyF2PB1sPrAg' \
+             b'fyea4RP8TiBb+GmDIA0ArF5h/CLUCPx5AKuISAsJJYIV4w8O8hAOj2O9VbTJRNn6YpAHT6nZxQnYun49nmQ' \
+             b'NTrXcSaKN8t37RRU85AMRvPEgDoXnZT8Pe3un31FXMymTzL/xwrXlA8n2MHdwPYAbB5AAAAAElFTkSuQmCC'
+    assert isinstance(qt.icon(base64), QtGui.QIcon)
+
+    default_size = qt.icon(QtWidgets.QStyle.SP_TitleBarMenuButton).availableSizes()[-1]
+    assert default_size.width() == 64  # the default size is chosen to be the largest QSize
+    assert default_size.height() == 64
 
     with pytest.raises(TypeError):
         qt.icon(None)
@@ -29,42 +34,82 @@ def test_icon():
     with pytest.raises(TypeError):
         qt.icon(99999)
 
-    with pytest.raises(FileNotFoundError):
-        qt.icon('cannot find this image')
+    with pytest.raises(TypeError):
+        qt.icon(bytearray(base64))
+
+    with pytest.raises(IOError):
+        qt.icon('this is not an image')
+
+    assert isinstance(qt.icon(os.path.join(os.path.dirname(__file__), 'gamma.png')), QtGui.QIcon)
+
+    try:
+        # make sure that the directory where the test_init.py file is not already in sys.path
+        # otherwise the qt.icon('gamma.png') test below will not raise the expected exception
+        sys.path.pop(sys.path.index(os.path.dirname(__file__)))
+    except ValueError:
+        pass
+
+    # expect IOError provided that os.path.dirname(__file__) is not in sys.path
+    with pytest.raises(IOError):
+        qt.icon('gamma.png')
+
+    # insert this directory back into sys.path and check again
+    sys.path.insert(0, os.path.dirname(__file__))
+    assert isinstance(qt.icon('gamma.png'), QtGui.QIcon)
+
+    if sys.platform == 'win32':
+        assert isinstance(qt.icon('C:/Windows/System32/shell32.dll|0'), QtGui.QIcon)
+        assert isinstance(qt.icon('shell32.dll|0'), QtGui.QIcon)
+        assert isinstance(qt.icon('shell32|0'), QtGui.QIcon)
+        with pytest.raises(IOError):
+            qt.icon('/shell32|0')  # it appears as though a full path is being specified
+        assert isinstance(qt.icon('C:/Windows/explorer.exe|0'), QtGui.QIcon)
+        assert isinstance(qt.icon('explorer.exe|0'), QtGui.QIcon)
+        assert isinstance(qt.icon('explorer|0'), QtGui.QIcon)
+        with pytest.raises(IOError):
+            qt.icon('C:/Windows/System32/explorer.exe|0')  # the exe is located at 'C:/Windows/explorer.exe'
+        with pytest.raises(IOError):
+            qt.icon('shell32|306')  # the maximum icon index is 305
 
 
-def test_image_to_bytes():
+def test_image_to_base64():
+    image = qt.icon('gamma.png')
+    default_size = QtCore.QSize(191, 291)
+    assert default_size.width() == 191
+    assert default_size.height() == 291
 
-    image = QtWidgets.QStyle.SP_TitleBarMenuButton
-    default_size = qt.icon(image).availableSizes()[-1]  # the default size is chosen as the largest QSize
-    assert default_size.width() == 64
-    assert default_size.height() == 64
+    assert isinstance(qt.image_to_base64(image), bytes)
 
-    assert isinstance(qt.image_to_bytes(image), bytes)
-
-    new_size = qt.icon( qt.image_to_bytes(image) ).availableSizes()[0]
+    new_size = qt.icon(qt.image_to_base64(image)).availableSizes()[0]
     assert new_size.width() == default_size.width()
     assert new_size.height() == default_size.height()
 
     factor = 2.6
-    new_size = qt.icon( qt.image_to_bytes(image, size=factor) ).availableSizes()[0]
+    new_size = qt.icon(qt.image_to_base64(image, size=factor)).availableSizes()[0]
     assert new_size.width() == int(default_size.width()*factor)
     assert new_size.height() == int(default_size.height()*factor)
 
-    width = 150
-    new_size = qt.icon( qt.image_to_bytes(image, size=width) ).availableSizes()[0]
-    assert new_size.width() == width
-    assert new_size.height() == width
+    y = 150
+    new_size = qt.icon(qt.image_to_base64(image, size=y)).availableSizes()[0]
+    assert new_size.width() == int(y*191./291.)
+    assert new_size.height() == y
+
+    x = 300
+    new_size = qt.icon(qt.image_to_base64(image, size=x, mode=QtCore.Qt.KeepAspectRatioByExpanding)).availableSizes()[0]
+    assert new_size.width() == x
+    assert new_size.height() == int(x*291./191.)
 
     size = (150, 200)
-    new_size = qt.icon( qt.image_to_bytes(image, size=size) ).availableSizes()[0]
-    assert new_size.width() == size[0]
-    assert new_size.height() == size[0]
-
-    new_size = qt.icon( qt.image_to_bytes(image, size=size, mode=QtCore.Qt.IgnoreAspectRatio) ).availableSizes()[0]
+    new_size = qt.icon(qt.image_to_base64(image, size=size, mode=QtCore.Qt.IgnoreAspectRatio)).availableSizes()[0]
     assert new_size.width() == size[0]
     assert new_size.height() == size[1]
 
-    new_size = qt.icon( qt.image_to_bytes(image, size=size, mode=QtCore.Qt.KeepAspectRatioByExpanding) ).availableSizes()[0]
-    assert new_size.width() == size[1]
+    new_size = qt.icon(qt.image_to_base64(image, size=size)).availableSizes()[0]
+    assert new_size.width() == int(size[1]*191./291.)
     assert new_size.height() == size[1]
+
+    for fmt in ['BMP', 'JPG', 'JPEG', 'PNG']:
+        qt.image_to_base64(image, fmt=fmt)
+
+    with pytest.raises(ValueError):
+        qt.image_to_base64(image, fmt='GIF')
