ab9534e5c533b08b74888559a06d5fc251fcb56f
jborbely <joe.borbely@gmail.com>
2017-08-27 14:39:33 +1200

    put logger example in a main() function

--- msl/examples/qt/logger.py
+++ msl/examples/qt/logger.py
@@ -7,25 +7,30 @@ import logging
 
 from msl.qt import application, Logger
 
-logger = logging.getLogger()
 
-app = application()
-log = Logger()
-log.setWindowTitle('Logger Widget')
+def main():
+    logger = logging.getLogger()
 
-for i in range(1000):
-    level = random.randint(10, 60)
-    if level < logging.INFO:
-        logger.debug('debug message {}'.format(i+1))
-    elif level < logging.WARNING:
-        logger.info('info message {}'.format(i+1))
-    elif level < logging.ERROR:
-        logger.warning('warning message {}'.format(i+1))
-    elif level < logging.CRITICAL:
-        logger.error('error message {}'.format(i+1))
-    else:
-        logger.critical('critical message {}'.format(i+1))
+    app = application()
+    log = Logger()
+    log.setWindowTitle('Logger Widget')
 
-log.resize(800, 400)
-log.show()
-sys.exit(app.exec())
+    for i in range(1000):
+        level = random.randint(10, 60)
+        if level < logging.INFO:
+            logger.debug('debug message {}'.format(i+1))
+        elif level < logging.WARNING:
+            logger.info('info message {}'.format(i+1))
+        elif level < logging.ERROR:
+            logger.warning('warning message {}'.format(i+1))
+        elif level < logging.CRITICAL:
+            logger.error('error message {}'.format(i+1))
+        else:
+            logger.critical('critical message {}'.format(i+1))
+
+    log.resize(800, 400)
+    log.show()
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
