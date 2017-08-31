d817a28fd82238c8ddcbdbce13025cf52dbd00b7
jborbely <joe.borbely@gmail.com>
2017-08-31 18:31:44 +1200

    add PyQt5.QtWidgets.QWidget to the mocked modules

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ if on_rtd:
         def __getattr__(cls, name):
             return MagicMock()
 
-    MOCK_MODULES = ['PyQt5', 'PyQt5.QtWidgets', 'PyQt5.QtCore', 'PyQt5.QtGui']
+    MOCK_MODULES = ['PyQt5', 'PyQt5.QtWidgets', 'PyQt5.QtCore', 'PyQt5.QtGui', 'PyQt5.QtWidgets.QWidget']
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 else:
