797f0879e671738184bb4024d1646f664174fa4f
jborbely <joe.borbely@gmail.com>
2018-01-22 20:07:47 +1300

    create a constants module

---
+++ msl/qt/constants.py
@@ -0,0 +1,7 @@
+"""
+Constants used by the **MSL-Qt** package.
+"""
+import os
+
+HOME_DIR = os.path.join(os.path.expanduser('~'), '.msl')
+""":obj:`str`: The default $HOME directory where all files are to be located."""
