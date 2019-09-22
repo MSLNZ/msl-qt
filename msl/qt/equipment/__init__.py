2c03548f7be27886461ce4ba4898e1af5cd1621b
jborbely <joe.borbely@gmail.com>
2019-09-22 20:56:06 +1200

    replace record.to_yaml() with repr(record)

--- msl/qt/equipment/__init__.py
+++ msl/qt/equipment/__init__.py
@@ -26,7 +26,7 @@ def show_record(record):
     widget = QtWidgets.QTextEdit()
     widget.setReadOnly(True)
     widget.setLineWrapMode(QtWidgets.QTextEdit.NoWrap)
-    widget.setText(record.to_yaml())
+    widget.setText(repr(record))
 
     hbox = QtWidgets.QHBoxLayout()
     hbox.addWidget(widget)
