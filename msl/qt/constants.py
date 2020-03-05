e34a59cc5f48acc27c46a94aa38608c019515d23
jborbely <joe.borbely@gmail.com>
2020-03-05 17:46:28 +1300

    create the number_to_si and si_to_number functions

--- msl/qt/constants.py
+++ msl/qt/constants.py
@@ -5,3 +5,6 @@ import os
 
 HOME_DIR = os.path.join(os.path.expanduser('~'), '.msl')
 """:class:`str`: The default ``$HOME`` directory where all files are to be located."""
+
+SI_PREFIX_MAP = {i: prefix for i, prefix in enumerate('yzafpn\u00b5m kMGTPEZY', start=-8)}
+""":class:`dict`: The SI prefixes used to form multiples of 10**(3*n), n=-8..8"""
