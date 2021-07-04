6b35bde6ac1c3bebf2908ff1ac5061fb76b4e657
jborbely <joe.borbely@gmail.com>
2021-07-04 18:37:44 +1200

    use sys.version_info.minor not sys.version_info.major

--- conftest.py
+++ conftest.py
@@ -2,6 +2,6 @@ import os
 import sys
 
 if 'QT_API' not in os.environ and not os.getenv('GITHUB_ACTIONS'):
-    os.environ['QT_API'] = 'PySide6' if sys.version_info.major > 5 else 'PySide2'
+    os.environ['QT_API'] = 'PySide6' if sys.version_info.minor > 5 else 'PySide2'
 
 sys.excepthook = print
